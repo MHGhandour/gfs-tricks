8f93f2842f3813ee790ee8fb48bb2cd205d04337
MHGhandour <55106045+MHGhandour@users.noreply.github.com>
2019-10-17 19:23:53 +0200

    Update vtk-gen.sh

--- vtk-gen.sh
+++ vtk-gen.sh
@@ -1,7 +1,7 @@
 for i in `seq 0 50 2000`
 do
 gunzip sim-$i-*
-gerris3D -e "GfsOutputSimulation {istep =1 } step-$i.vtk { format = VTK variables  = Vorticity}" sim-$i-* > /dev/null
+gerris3D -e "GfsOutputSimulation {istep =1 } step-$i.vtk { format = VTK variables  = T1,P,U,V,W}" sim-$i-* > /dev/null
 gzip sim-$i-*
 done
 
