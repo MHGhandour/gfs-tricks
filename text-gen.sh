603e86142878aabc11c807543863487eec2240c8
MHGhandour <55106045+MHGhandour@users.noreply.github.com>
2019-10-17 19:20:01 +0200

    Add files via upload

--- text-gen.sh
+++ text-gen.sh
@@ -1,7 +1,7 @@
 for i in `seq 0 50 2000`
 do
 gunzip sim-$i-*
-gerris3D -e "GfsOutputSimulation {istep =1 } step-$i.vtk { format = text }" sim-$i-* > /dev/null
+gerris3D -e "GfsOutputSimulation {istep =1 } step-$i.txt { format = text }" sim-$i-* > /dev/null
 gzip sim-$i-*
 done
 
